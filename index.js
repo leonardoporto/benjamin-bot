4c5f62c2635921d0a720e453b0c5af1d3d3e52ff
Leonardo Porto <leonardo.porto.souza@gmail.com>
2019-04-06 16:13:00 -0300

    Comunicação com o telegram

---
+++ index.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const Telegraf = require('telegraf/telegram');
+const express = require('express');
+const _ = require('lodash');
+
+const app = express();
+
+app.use(express.json())
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+/**
+ * @description Envia mensagem a um grupo no telegram.
+ */
+app.post('/group/:group', (req, res) => {
+
+    const {
+        params,
+        body
+    } = req;
+    
+    // template de mensagens
+    const templates = {
+        "merge_request": "[${object_attributes.action}] ${object_attributes.assignee.name}  Merge Request\n${object_attributes.url}",
+        "note": "Novo comentário em um merge request\n${object_attributes.url}"
+    }
+    
+    const message = _.template(templates[body.object_kind]);
+
+    bot.sendMessage(params.group, message(body))
+        .then(result => {
+            console.log(result);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+    res.status(200).send('Ok');
+
+});
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!')
+});
